52b2f38a650281468e38e53aca3431e552aa7eae
Tim Phan <tim.phan@digitalent.se>
2020-01-30 15:46:03 +0100

    Initial commit

---
+++ scheduledTrigger/index.js
@@ -0,0 +1,61 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const _SITEURL_ = "https://wahlinfastigheter.se/lediga-objekt/lagenheter/";
+
+module.exports = async function(context, myTimer) {
+  var timeStamp = new Date().toISOString();
+
+  if (myTimer.isPastDue) {
+    context.log("JavaScript is running late!");
+  }
+  const target = await targetDocument();
+  console.log("result");
+  console.log(target);
+  // const scan = scanDocument();
+  context.log("JavaScript timer trigger function ran successfully!", timeStamp);
+};
+
+// Scan document with cheerio
+async function targetDocument() {
+  const $ = await fetchData();
+  let availableApartments = 0;
+  $(".ojects-term-list li a").map((i, el) => {
+    if (i === 1) {
+      const text = $(el)
+        .text()
+        .trim();
+      const num = text.charAt(0);
+      if (isNaN(num)) {
+        console.log(`Text is not a number: ${num}`);
+        return;
+      }
+      availableApartments = num;
+    }
+  });
+  if (Number(availableApartments) > 0) {
+    return Promise.resolve({
+      availableApartments: Number(availableApartments)
+    });
+  }
+  return Promise.resolve({
+    availableApartments: 0
+  });
+}
+
+// Scan the document body using for example regEx
+async function scanDocument() {
+  const $ = await fetchData();
+  const htmlDocument = $.html();
+  console.log("scan");
+  // console.log(htmlDocument);
+  return "";
+}
+
+// Fetch site and load with cheerio
+const fetchData = async () => {
+  const result = await axios.get(_SITEURL_);
+  return cheerio.load(result.data);
+};
+
+// "0 35 13 * * 1-5"
---
+++ scheduledTrigger/readme.md
@@ -0,0 +1,11 @@
+# What is it
+
+Utilizing TimerTrigger for Azure functions to scrape a website for its content on a schedule.
+
+## How it works
+
+TimerTrigger works by providing a schedule in the form of a [cron expression](https://en.wikipedia.org/wiki/Cron#CRON_expression)(See the link for full details).
+
+- Change the schedule in function.json
+- Simple fetch with axios and use Cheerio-API for traversing the resulting data structure
+- I used this to receive mail-notifications when new apartments were announced at a website
